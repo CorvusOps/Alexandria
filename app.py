849dea9f10710652c697b6364c190f598b5d9c40
CorvusOps <josepholivar124@gmail.com>
2023-07-01 22:01:01 +0800

    Everything anything all at once

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
 import os, pickle
+from ssl import AlertDescription
+
+from bson.objectid import ObjectId
+
+# from flask_pymongo import PyMongo
+
+from pprint import pprint
+
 from predictions import classification_process
 
-from flask import Flask, render_template, request
+from flask import Flask, flash,render_template, request, redirect, url_for
+
+from pymongo import MongoClient
 
 # Create object using flask
 app = Flask(__name__)
+app.secret_key = "alexandrea"
+
+## This is for PyMongo Config of flask_pymongo
+# app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+# app.config["MONGO_URI"] = 'mongodb://localhost:27017'
+# mongo = PyMongo(app)
+# pymongo = PyMongo(app)
+
+## This is MongoClient config of pymongo
+# # This URI connects in which mongodb server you are located, first is in the atlas (web), second is local
+# uri = "mongodb+srv://jfsolivar:rw3wplVA6701T4gd@alexandria.9mrd2oe.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb://localhost:27017"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client['lms-dev']
+records = db.books
+
+## This is for ensuring the connection of the database
+# # List all the databases in the cluster:
+# for db_info in client.list_database_names():
+#    print(db_info)
+
+# # List all the collections in 'lms_dev':
+# collections = db.list_collection_names()
+# for collection in collections:
+#    print(collection)
+
+# # Get a reference to the 'books' collection: 
+# books = db['books']
+
+# # Get a document with the genre 'fantasy':
+# pprint(books.find_one({'genre': 'fantasy'}))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 
 # Load the TF-IDF vectorizer and svm model
 load_vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
@@ -12,15 +63,24 @@ svm_model = pickle.load(open('models/svm-76-accuracy.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    some_books = records.aggregate([{"$sample":{"size":6}}])
+
+    return render_template("home.html", some_books = some_books)
 
 @app.route('/sidebar')
 def sidebar():
     return render_template("sidebar.html")
 
+@app.route('/<id>/delete')
+def delete(id):
+    records.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('books'))
+
 @app.route('/books')
 def books():
-    return render_template("books.html")
+    all_books = records.find()
+    return render_template("books.html", all_books=all_books)
+
 
 @app.route('/analytics')
 def analytics():
@@ -30,9 +90,33 @@ def analytics():
 def genre_checker():
     return render_template("genre-checker.html")
 
-@app.route('/add-books')
+@app.route('/add-books', methods=["GET", "POST"])
 def add_books():
-    return render_template("add-books.html")
+        # For GET request: Display form for user to submit report of UFO sighting
+    if request.method == "GET":
+        return render_template("add-books.html")
+    
+    books = {}
+
+    book_title = request.form.get("book-title")
+    book_author = request.form.get("book-author")
+    pub_year = request.form.get("pub-year")
+    summary = request.form.get("summary")
+    genre = request.form.get("genre")
+    
+
+    books["title"] = book_title
+    books["book_author"] = book_author
+    books["pub_year"] = pub_year
+    books["summary"] = summary
+    books["genre"] = genre
+
+    pprint(books)
+    records.insert_one(books)
+
+    flash('You successfully added a new book')
+
+    return redirect(url_for('books'))
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
